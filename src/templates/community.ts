57589bcdcb93e66682aa5f803abb45590879812f
Cheteck <6423993+Cheteck@users.noreply.github.com>
2026-05-07 20:21:21 +0100

    feat: update src/templates/community.ts

--- src/templates/community.ts
+++ src/templates/community.ts
@@ -1,10 +1,32 @@
 export const communityTemplate = {
   name: 'Community',
-  description: 'Standard template for online communities and groups',
+  description: 'Standard template for online communities and groups focusing on social link',
   defaultRoles: {
-    ADMIN: ['space.manage', 'space.update', 'space.delete', 'member.add', 'member.remove', 'member.update_role', 'post.manage', 'post.delete'],
-    MODERATOR: ['space.view', 'member.list', 'member.kick', 'post.delete', 'post.pin'],
-    MEMBER: ['space.view', 'member.list', 'post.create', 'post.comment'],
-    VISITOR: ['space.view']
+    guardian: [
+      'space.manage',
+      'space.update',
+      'space.delete',
+      'member.add',
+      'member.remove',
+      'member.update_role',
+      'post.manage',
+      'post.delete'
+    ],
+    facilitator: [
+      'space.view',
+      'member.list',
+      'member.kick',
+      'post.delete',
+      'post.pin'
+    ],
+    contributor: [
+      'space.view',
+      'member.list',
+      'post.create',
+      'post.comment'
+    ],
+    supporter: [
+      'space.view'
+    ]
   }
 };
